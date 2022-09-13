ea42f6c1aea2431a24a9ac0ee23983edaf6706a7
0xbahamoot <lam@incognito.org>
2022-09-13 15:24:28 +0700

    fix

--- src/http/pancake-swap/constants.ts
+++ src/http/pancake-swap/constants.ts
@@ -429,6 +429,6 @@ export const BSC_MAINNET_CONFIGS = {
   incBSCContractAddress: '0x43D037A562099A4C2c95b1E2120cc43054450629',
 };
 export const BSC_TESTNET_CONFIGS = {
-  host: "https://data-seed-prebsc-2-s3.binance.org:8545",
+  host: String(process.env.JSON_RPC_PROVIDER_POLYGON),
   incBSCContractAddress: "0x43D037A562099A4C2c95b1E2120cc43054450629",
 };
